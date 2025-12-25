89704ca179cb75a1b4fc1040a75d1e709ace9410
Kai dev IT <thangtrandz04@gmail.com>
2025-12-25 16:17:18 +0700

    feat: update logic overview-content/Tổng quan

--- src/pages/user/infor-account/components/tab-content/overview-content.tsx
+++ src/pages/user/infor-account/components/tab-content/overview-content.tsx
@@ -1,10 +1,16 @@
-import WishListProducts from "@/components/user/WishListProducts";
+"use client";
+
 import { useCopyToClipboard } from "@/hooks";
 import OrdersPage from "@/pages/user/orders/page";
+import ProductCard from "@/pages/user/public/ProductCard";
+import useStore from "@/store/store";
 import { Badge } from "@/ui/badge";
+import SeeMore from "@/ui/see-more";
 import { Button } from "antd";
+
 export function OverviewContent() {
   const { copyFn } = useCopyToClipboard();
+  const { favoriteProduct } = useStore();
 
   return (
     <div className="space-y-6">
@@ -34,31 +40,38 @@ export function OverviewContent() {
         </div>
       </div>
 
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 gap-3 lg:grid-cols-3">
         {/* Recent Orders */}
-        <div className="col-span-2 rounded-lg shadow-sm">
+        <div className="col-span-1 lg:col-span-2 rounded-lg shadow-sm">
           <div className="flex items-center justify-between mb-4">
             <h2 className="text-lg font-semibold">Đơn hàng gần đây</h2>
           </div>
-            <OrdersPage hideTitle />
+          <OrdersPage hideTitle />
         </div>
 
         {/* Your Benefits */}
-        <div className=" rounded-lg shadow-sm">
+        <div className="rounded-lg shadow-sm lg:col-span-1">
           <h2 className="text-lg font-semibold mb-4">Ưu đãi của bạn</h2>
           <div className="border border-border rounded-lg p-4">
-            <h3 className="font-medium mb-2">[EMAIL] ƯU ĐÃI KHÁCH HÀNG...</h3>
+            <h3 className="font-medium mb-2 truncate">
+              [EMAIL] ƯU ĐÃI KHÁCH HÀNG...
+            </h3>
             <p className="text-sm text-foreground mb-2">
               Giảm giá: <span className="font-medium">0đ</span>
             </p>
             <p className="text-sm text-foreground mb-2">
               HSD: <span className="font-medium">03/01/2028</span>
             </p>
-            <div className="flex items-center gap-2 mt-3">
-              <Badge variant={'success'}>
+
+            {/* Badge + Button */}
+            <div className="flex flex-col md:flex-col lg:flex-row lg:items-center gap-2 mt-3">
+              <Badge variant={"success"} className="w-full lg:w-auto">
                 EMAIL_DGFBG15
               </Badge>
-               <Button className="mt-2 sm:mt-0" onClick={() => copyFn('EMAIL_DGFBG15')}>
+              <Button
+                className="w-full md:w-full lg:w-auto"
+                onClick={() => copyFn("EMAIL_DGFBG15")}
+              >
                 Sao chép
               </Button>
             </div>
@@ -66,8 +79,22 @@ export function OverviewContent() {
         </div>
       </div>
 
-      <div className="border-y py-4">
-          <WishListProducts />
+      <div className="w-full">
+        <div className="flex items-center justify-between mb-3">
+          <h2 className="text-lg font-semibold">Sản phẩm yêu thích</h2>
+
+          {favoriteProduct && favoriteProduct.length > 3 && (
+            <SeeMore to="/wishlist">Xem tất cả</SeeMore>
+          )}
+        </div>
+
+        <div className="border-y py-4 overflow-x-auto">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {favoriteProduct?.slice(0, 3).map((item: any) => (
+              <ProductCard key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
